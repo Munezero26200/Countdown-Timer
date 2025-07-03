b8dcdfa85664b37dd070d6cb277826ca43cf33c8
Munezero26200 <alinemunezero920@gmail.com>
2025-07-03 15:18:12 +0200

    Here is countdown timer so far

--- countdown.js
+++ countdown.js
@@ -1,20 +1,34 @@
-const countdown= document.getElementById("countdown");
+const countdown = document.getElementById("countdown");
+const startButton = document.getElementById("startBtn");
 
-const targetDate= new Date("December 31, 2025 23:59:59").getTime();
-//create interval
-const timer= setInterval(()=>{
-    const now= new Date().getTime();
-    const difference= targetDate - now;
+let timer;
 
-    const days= Math.floor(difference/(1000*60*60*24));
-    const hours= Math.floor((difference%(1000*60*60*24))/(1000*60*60));
-    const minutes= Math.floor((difference%(1000*60*60))/(1000*60));
-    const seconds= Math.floor((difference % (1000*60))/1000);
-
-    countdown.textContent= `${days}d ${hours}h ${minutes}m ${seconds}s`
-
-},1000);
-if(difference <=0){
+startButton.addEventListener('click', () => {
+    const userInput = document.getElementById("targetDate").value;
+    console.log("user selected:", userInput);
+    if (!userInput) {
+        countdown.textContent = "⛔ please enter a valid date/time.";
+        return;
+    }
+    const targetTime = new Date(userInput).getTime();
     clearInterval(timer);
-    countdown.textContent="🎊🎊🎂My brother bithday!"
-}
+
+    timer = setInterval(() => {
+        const now = new Date().getTime();
+        const difference = targetTime - now;
+        if (difference <= 0) {
+            clearInterval(timer);//stop timer
+            countdown.textContent = "🎊🎊it's time";
+            return;
+        }
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        countdown.textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }, 1000);
+})
+
+
+
